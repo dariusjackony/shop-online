a9a8d20f68d6ab3e6c042f7e4d673663dd973cc7
darius <dariusjackon11@gmail.com>
2026-01-18 14:13:18 +0300

    Update:Initial Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,40 +1,38 @@
+import LOGO from "../assets/logo.jpg";
 import { useState } from "react";
+
 export default function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
   const [open, setOpen] = useState(false);
 
   return (
-    <nav className="bg-white shadow-md fixed w-full z-50">
-      <div className="max-w-7xl mx-auto px-4">
-        <div className="flex justify-between items-center h-16">
-
-          {/* Logo */}
-          <div className="text-xl font-bold text-black">
-            MyNGO
-          </div>
-
-          {/* Desktop Menu */}
-          <div className="hidden md:flex items-center space-x-8 font-semibold">
-            <a href="">Home</a>
-            </div>
-            <a href="#" className="text-gray-700 ">
-              News
-            </a>
-            <a href="#" className="text-gray-700 ">
-              Work With Us
-            </a>
-          </div>
-
-          {/* Mobile Menu Button */}
-          <div className="md:hidden">
-            <button
-              onClick={() => setIsOpen(!isOpen)}
-              className="text-gray-700 focus:outline-none font-bold text-2xl"
-            >
-              ☰
-            </button>
-          </div>
+    <nav className="bg-white shadow-md p-4">
+      <div className="flex justify-between items-center">
+        <div>
+          <img src={LOGO} className="w-25" />
         </div>
+        <div>
+          <input type="text" placeholder="search for product" className="w-40 p-1 rounded-lg mr-3" />
+        </div>
+        <div className="hidden md:flex space-x-4">
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+          <a href="/shop">Shop</a>
+          <a href="/contact">Contact</a>
+        </div>
+
+        <button className="md:hidden text-xl bg-gray-900 text-white p-1 rounded-lg" onClick={() => setOpen(!open)}>
+          {open ? "X" : "☰"}
+        </button>
+      </div>
+
+      {open && (
+        <div className="flex flex-col mt-2 space-y-2 md:hidden">
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+          <a href="/shop">Shop</a>
+          <a href="/contact">Contact</a>
+        </div>
+      )}
     </nav>
   );
 }
